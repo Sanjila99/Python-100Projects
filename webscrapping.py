69cc89b90904793826a83882c859869ca3ebbe29
Sanjila99 <canzilagangaju@gmail.com>
2024-03-01 21:41:57 +0545

    changed the git struct for image link

--- webscrapping.py
+++ webscrapping.py
@@ -5,5 +5,5 @@ github_user=input("Input Giithub Username:")
 url="https://github.com/"+github_user
 r=requests.get(url)
 soup=bs(r.content,'html.parser')
-profile_image=soup.find('img',{'alt':'Avatar'})['src']
+profile_image=soup.find('img', class_='avatar-user')['src']
 print(profile_image)
