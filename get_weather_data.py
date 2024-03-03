0edf96146e94511f7c79ee53a57d6e0f8d0acb08
Sanjila99 <canzilagangaju@gmail.com>
2024-03-03 16:00:41 +0545

    API

--- get_weather_data.py
+++ get_weather_data.py
@@ -1,7 +1,7 @@
 import requests
 from pprint import pprint
 
-API_Key= '4010ba45befc1979fbcf1417fb6480c8'
+API_Key= ''
 city=input("Enter a city:")
 base_url="http://api.openweathermap.org/data/2.5/weather?appid=" + API_Key + "&q="+city
 weather_data=requests.get(base_url).json()
