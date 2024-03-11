7de3d932ded58146700d2f0c7d7d45016f58bd4c
Sanjila99 <canzilagangaju@gmail.com>
2024-03-11 20:31:03 +0545

    bmi features

--- chattybot.py
+++ chattybot.py
@@ -134,6 +134,22 @@ def creator():
         print("Oops! You unlucky one u couldnot know the greatest divine person!")
     else:
         print("answer me yes or no !!bro")
+def bmi():
+    user_weight=float(input("Enter your body weight in kilograms:"))
+    user_height=float(input("Enter your height in centimeters:"))
+    user_height=user_height/100
+    BMI=user_weight/(user_height*user_height)
+    print("Your Body Mass Index :",BMI)
+    if BMI<18.5:
+     print("Underweight.")
+    elif 18.5<BMI<24.9:
+      print("Normal Weight")
+    elif 25<BMI<29.9:
+     print("Overweight")
+    elif BMI>=30:
+      print("Obese")
+    else:
+      print("Enter Valid Details")
           
 
 
@@ -148,5 +164,6 @@ end()
 end_time=time.time()
 print(f"Hola! We talked for {end_time-start_time:.3f} seconds")
 creator()
+bmi()
  
     
